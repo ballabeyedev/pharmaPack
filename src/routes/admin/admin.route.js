41566928074a860b93b01d9b5af5887aaf870cad
Balla221 <balla.beye_220540@groupeiam.com>
2026-03-28 20:52:30 +0000

    Ajout fonctionnalite activer utilisateur

--- src/routes/admin/admin.route.js
+++ src/routes/admin/admin.route.js
@@ -6,9 +6,9 @@ const isAdmin = require('../../middlewares/isAdmin.middleware');
 const checkActiveUser = require('../../middlewares/checkActiveUser.middleware');
 
 // Middleware commun pour toutes les routes admin
-router.use(auth);             // Vérifie que l'utilisateur est connecté
-router.use(checkActiveUser);  // Vérifie que le compte est actif
-router.use(isAdmin);          // Vérifie que c'est un admin
+//router.use(auth);             // Vérifie que l'utilisateur est connecté
+//router.use(checkActiveUser);  // Vérifie que le compte est actif
+//router.use(isAdmin);          // Vérifie que c'est un admin
 
 // -------------------- ROUTES ADMIN --------------------
 
