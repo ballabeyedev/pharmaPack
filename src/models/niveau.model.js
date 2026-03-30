6c2efd4960bf79f24264f43b243ffbe16ce7ae54
Balla221 <balla.beye_220540@groupeiam.com>
2026-03-30 12:53:21 +0000

    Affichage les statistique du dashboard admin

--- src/models/niveau.model.js
+++ src/models/niveau.model.js
@@ -22,7 +22,7 @@ const Niveau = sequelize.define('Niveau', {
 }, {
   tableName: 'niveaux',
   timestamps: true,
-  createdAt: created_at,
+  createdAt: 'created_at', 
   updatedAt: 'updated_at',
   underscored: true
 });
